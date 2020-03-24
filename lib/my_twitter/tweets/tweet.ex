0a7bb46d10c018ff1f3ff0701551ee4acfade2ec
newtonjose <josephnewton001@gmail.com>
2020-03-24 06:55:43 -0300

    tweet default fields values

--- lib/my_twitter/tweets/tweet.ex
+++ lib/my_twitter/tweets/tweet.ex
@@ -2,27 +2,42 @@ defmodule MyTwitter.Tweets.Tweet do
   use Ecto.Schema
   import Ecto.Changeset
 
+  alias MyTwitter.Account.User
 
   schema "tweets" do
-    field :created_at, :string
-    field :favorite_count, :integer
+    field :created_at, :string, default: NaiveDateTime.to_string(NaiveDateTime.utc_now())
+    field :favorite_count, :integer, default: 0
     field :favorited, :boolean, default: false
     field :in_reply_to_screen_name, :string
     field :in_reply_to_status_id, :integer
     field :in_reply_to_user_id, :integer
-    field :reply_count, :integer
-    field :retweet_count, :integer
+    field :reply_count, :integer, default: 0
+    field :retweet_count, :integer, default: 0
     field :retweeted, :boolean, default: false
     field :text, :string
-    field :user_id, :id
 
+    belongs_to :user, User
     timestamps()
   end
 
   @doc false
   def changeset(tweet, attrs) do
     tweet
-    |> cast(attrs, [:text, :reply_count, :retweet_count, :favorite_count, :favorited, :retweeted, :in_reply_to_status_id, :in_reply_to_user_id, :in_reply_to_screen_name, :created_at])
-    |> validate_required([:text, :reply_count, :retweet_count, :favorite_count, :favorited, :retweeted, :in_reply_to_status_id, :in_reply_to_user_id, :in_reply_to_screen_name, :created_at])
+    |> cast(attrs, [
+      :text,
+      :reply_count,
+      :retweet_count,
+      :favorite_count,
+      :favorited,
+      :retweeted,
+      :in_reply_to_status_id,
+      :in_reply_to_user_id,
+      :in_reply_to_screen_name,
+      :created_at
+    ])
+    |> validate_required([
+      :text
+    ])
+    |> validate_length(:text, max: 140)
   end
 end
