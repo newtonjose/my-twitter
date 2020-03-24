c27172e05b59f2fe4b12981801fec4366c04c17f
newtonjose <josephnewton001@gmail.com>
2020-03-24 04:22:09 -0300

    session to check user

--- lib/my_twitter_web/endpoint.ex
+++ lib/my_twitter_web/endpoint.ex
@@ -1,33 +1,59 @@
 defmodule MyTwitterWeb.Endpoint do
   use Phoenix.Endpoint, otp_app: :my_twitter
-  use Absinthe.Phoenix.Endpoint
-  use NewRelic.Transaction
 
-  origin =
-    case Mix.env() do
-      :prod -> ["https://my_twitter-demo.herokuapp.com"]
-      _ -> false
-    end
+  socket("/socket", MyTwitterWeb.UserSocket)
 
-  socket "/socket", MyTwitterWeb.AbsintheSocket,
-    websocket: [check_origin: origin],
-    longpoll: true
+  # Serve at "/" the static files from "priv/static" directory.
+  #
+  # You should set gzip to true if you are running phoenix.digest
+  # when deploying your static files in production.
+  plug(Plug.Static,
+    at: "/",
+    from: :my_twitter,
+    gzip: false,
+    only: ~w(css fonts images js favicon.ico robots.txt)
+  )
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
   if code_reloading? do
-    plug Phoenix.CodeReloader
+    plug(Phoenix.CodeReloader)
   end
 
-  plug Plug.RequestId
-  plug Plug.Logger
+  plug(Plug.Logger)
 
-  plug Plug.Parsers,
+  plug(Plug.Parsers,
     parsers: [:urlencoded, :multipart, :json],
     pass: ["*/*"],
-    json_decoder: Phoenix.json_library()
+    json_decoder: Poison
+  )
 
-  plug CORSPlug
+  plug(Plug.MethodOverride)
+  plug(Plug.Head)
 
-  plug MyTwitterWeb.Router
+  # The session will be stored in the cookie and signed,
+  # this means its contents can be read but not tampered with.
+  # Set :encryption_salt if you would also like to encrypt it.
+  plug(Plug.Session,
+    store: :cookie,
+    key: "_my_twitter_key",
+    signing_salt: "dTQOW9Tv"
+  )
+
+  plug(MyTwitterWeb.Router)
+
+  @doc """
+  Callback invoked for dynamically configuring the endpoint.
+
+  It receives the endpoint configuration and checks if
+  configuration should be loaded from the system environment.
+  """
+  def init(_key, config) do
+    if config[:load_from_system_env] do
+      port = System.get_env("PORT") || raise "expected the PORT environment variable to be set"
+      {:ok, Keyword.put(config, :http, [:inet6, port: port])}
+    else
+      {:ok, config}
+    end
+  end
 end
