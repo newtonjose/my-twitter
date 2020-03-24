4691d1b4cdb11d01fd9e504c109b742286625f9c
newtonjose <josephnewton001@gmail.com>
2020-03-24 06:29:30 -0300

    fix missed adapter

--- lib/my_twitter/repo.ex
+++ lib/my_twitter/repo.ex
@@ -1,5 +1,7 @@
 defmodule MyTwitter.Repo do
-  use Ecto.Repo, otp_app: :my_twitter
+  use Ecto.Repo,
+    otp_app: :my_twitter,
+    adapter: Ecto.Adapters.Postgres
 
   @doc """
   Dynamically loads the repository url from the
